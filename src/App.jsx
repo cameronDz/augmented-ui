77a6587feb8195b608ce1ed25dc5f6fb3b550816
Cameron Dziurgot <camerondziurgot@gmail.com>
2018-10-11 01:24:13 -0400

    Added base for dropdown.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Routine from './Routine';
 import ExerciseList from './ExerciseList';
+import ExerciseDropdown from './ExerciseDropdown';
 
 class App extends Component {
 
 	render() {
 		return (
 			<div className="App">
+				<ExerciseDropdown />
 				<Routine />
 				<ExerciseList />
 			</div>
---
+++ src/ExerciseDropdown.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+
+class Exercise extends Component {
+
+	render() {
+		return (
+			{this.props.Name}
+		)
+	}
+}
+
+class ExerciseDropdown extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			listOpen: false,
+			headerTitle: "",
+			exercises: []
+		};
+	}
+
+	handleClickOutside(e) {
+		this.setState({
+			listOpen: false
+		})
+	}
+
+	selectItem = (title, id, stateKey) => {
+		this.setState({
+			headerTitle: title,
+			listOpen: false,
+		}, 
+		this.props.resetThenSet(id, stateKey))
+	}
+
+	toggleList = () => {
+		this.setState(prevState => ({
+			listOpen: !prevState.listOpen
+		}))
+	}
+
+	
+
+	componentDidMount() {
+		fetch('https://augmentedaspnetbackend.azurewebsites.net/v0.2/api/exercises')
+			.then(response => response.json())
+			.then(data => this.setState({
+				exercises: data
+			}));
+	}
+
+	render() {
+		const exerciseComponent = this.state.exercises.map(exerciseObject => {
+			return (
+				<Exercise {...exerciseObject} />
+			)
+		})
+
+		return (
+			<div className="ExerciseDropdown">
+				<h1>Exercise Dropdown</h1>
+				<p>Name</p>
+				      <div className="dd-wrapper">
+        <div className="dd-header" onClick={this.toggleList}>
+          <div className="dd-header-title">{headerTitle}</div>
+          {listOpen
+            ? <FontAwesome name="angle-up" size="2x"/>
+            : <FontAwesome name="angle-down" size="2x"/>
+          }
+        </div>
+        {listOpen && <ul className="dd-list">
+          {list.map((item)=> (
+            <li className="dd-list-item" key={item.id} onClick={() => this.selectItem(item.title, item.id, item.key)}>{item.title} {item.selected && <FontAwesome name="check"/>}</li>
+          ))}
+        </ul>}
+      </div>
+    )
+
+				
+			</div>
+		)
+	}
+}
+
+export default ExerciseDropdown;
