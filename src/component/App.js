4e1f85909a8d7f01a117c3f6a7877cbdb7f0911a
Cameron Dziurgot <camerondziurgot@gmail.com>
2018-10-12 23:35:03 -0400

    Added Dropdown component and sample.

---
+++ src/component/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Dropdown from './components/Dropdown';
+
+class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {exercise:[]};     
+  }
+    
+  componentDidMount() {
+    function  processExercise(payLoad)  {
+      var array = [];
+      for(var i = 0; i < payLoad.length; i++) {
+        var counter = payLoad[i];
+        var obj = {};
+        obj.id = counter.exerciseId;
+        obj.title = counter.name;
+        obj.key = 'exercise';
+        obj.selected = false;
+        array.push(obj);
+      }
+      return array;
+    }
+
+    fetch('https://augmentedaspnetbackend.azurewebsites.net/v0.2/api/exercises')
+      .then(response => response.json())
+      .then(data => this.setState({
+        exercise : processExercise(data)
+      })
+    );
+  }
+
+  toggleSelected = (id, key) => {
+    let temp = [...this.state[key]]
+    temp[id].selected = !temp[id].selected
+    this.setState({
+      [key]: temp
+    })
+  }
+
+  resetThenSet = (id, stateKey) => {
+    let exercises = [...this.state.exercise]
+    exercises.forEach(item => item.selected = false);
+    exercises[id].selected = true;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Dropdown menu examples</p>
+	<div>
+          <Dropdown
+	    title="Testing Exercises"
+	    list={this.state.exercise}
+	    resetThenSet={this.resetThenSet}
+	  />
+	</div>
+	
+      </div>
+    );
+  }
+}
+
+export default App;
