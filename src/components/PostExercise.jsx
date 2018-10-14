ace17b2e86d9d79f2bc19b0f5d72d1439fc34bbb
cameronDz <camerondziurgot@gmail.com>
2018-10-14 02:02:17 -0400

    Added inputform fields.

--- src/components/PostExercise.jsx
+++ src/components/PostExercise.jsx
@@ -3,13 +3,37 @@ import React, { Component } from 'react';
 class PostExercise extends Component {
 
 	constructor(props) {
-		super(props)
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.name = React.createRef();
+		this.description = React.createRef();
+	}
+
+	handleSubmit(event) {
+		alert('A name was submitted: ' + this.name.current.value);
+		alert('A description was submitted: ' + this.description.current.value);
+		event.preventDefault();
 	}
 
 	render() {
 		return (
 			<div>Under construction</div>
-		)
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					Name:
+                    <input
+						defaultValue=""
+						type="text"
+						ref={this.name} />
+					Description:
+                    <input
+						defaultValue=""
+						type="text"
+						ref={this.description} />
+				</label>
+				<input type="submit" value="Submit" />
+			</form>
+        );
 	}
 }
 export default PostExercise;
