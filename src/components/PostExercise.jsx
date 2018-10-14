5b9e50e1f7cadd8f98a8bbde0042b706a76573ef
Cameron Dziurgot <camerondziurgot@gmail.com>
2018-10-14 13:24:17 -0400

    Added an attempt at posting to azure.

--- src/components/PostExercise.jsx
+++ src/components/PostExercise.jsx
@@ -1,43 +1,66 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class PostExercise extends Component {
 
-	constructor(props) {
-		super(props);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.name = React.createRef();
-		this.description = React.createRef();
-	}
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.name = React.createRef();
+    this.description = React.createRef();
+    this.typeId = React.createRef();
+  }
 
-	handleSubmit(event) {
-		alert('A name was submitted: ' + this.name.current.value);
-		alert('A description was submitted: ' + this.description.current.value);
-		event.preventDefault();
-	}
+  handleSubmit(event) {
+    event.preventDefault();
+   
+    const payload = JSON.stringify({
+      name: this.name.current.value,
+      description: this.description.current.value,
+      typeId: this.typeId.current.value
+    });; 
 
-	render() {
-		return (
-			<div>
-			<p>Under construction</p>
-			<form onSubmit={this.handleSubmit}>
-				<label>
-					Name:
-                    <input
-						defaultValue=""
-						type="text"
-						ref={this.name} />
-			</label>
-			<label>
-					Description:
-                    <input
-						defaultValue=""
-						type="text"
-						ref={this.description} />
-				</label>
-				<input type="submit" value="Submit" />
-			</form>
-			</div>
-        );
-	}
+    // TODO fix, not POSTing
+    axios({
+      method: 'post',
+      url: 'https://augmentedaspnetbackend.azurewebsites.net/v0.2/api/exercises',
+      data: payload,
+      headers: {'Accepts': 'application/json', 'Content-Type': 'text/json'},
+    });
+ 
+    alert('POST data NOT sent. ' + payload);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Under construction</p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              defaultValue=""
+              type="text"
+              ref={this.name} />
+          </label>
+          <label>
+            Description:
+            <input
+              defaultValue=""
+              type="text"
+              ref={this.description} />
+          </label>
+	  <label>
+            Type:
+            <input
+              defaultValue="1"
+              type="number"
+              ref={this.typeId} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
 export default PostExercise;
