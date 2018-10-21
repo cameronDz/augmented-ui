9d154904f414ac05a7c27ccf14228a1f09ba959c
Cameron Dziurgot <camerondziurgot@gmail.com>
2018-10-20 22:36:53 -0400

    Added Nutrition tabs to navbar.

--- src/components/navbar/NavbarStart.jsx
+++ src/components/navbar/NavbarStart.jsx
@@ -7,8 +7,21 @@ class NavbarStart extends Component {
     return (
       <div className="navbar-start">
         <a className="navbar-item">Home</a>
-        <a className="navbar-item">Exercises</a>
-        <a className="navbar-item">Sessions</a>
+        <div className="navbar-item has-dropdown is-hoverable">
+          <a className="navbar-link">Fitness</a>
+          <div className="navbar-dropdown">
+            <a className="navbar-item">Exercises</a>
+            <a className="navbar-item">Sessions</a>
+          </div>
+        </div>
+        <div className="navbar-item has-dropdown is-hoverable">
+          <a className="navbar-link">Nutrition</a>
+          <div className="navbar-dropdown">
+            <a className="navbar-item">Diet</a>
+            <a className="navbar-item">Meals</a>
+            <a className="navbar-item">Food</a>
+          </div>
+        </div>
         <div className="navbar-item has-dropdown is-hoverable">
           <a className="navbar-link">More</a>
           <div className="navbar-dropdown">
