8bb8aaf29d8cd542bdc70945f23fb4038d06da42
Cameron Dziurgot <camerondziurgot@gmail.com>
2018-10-25 03:00:38 -0400

    Added route for home page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 
 class App extends Component {
@@ -6,7 +7,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <HomePage />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+        </Switch>
       </div>
     );
   }
