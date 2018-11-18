3a8046524b95f109cbe5d1f776761cfab0773dd6
cameronDz <camerondziurgot@gmail.com>
2018-11-18 03:14:09 -0500

    Removed handlers. Updated inputs.

--- src/components/CardioMachineCreator.jsx
+++ src/components/CardioMachineCreator.jsx
@@ -7,12 +7,6 @@ class CardioMachineCreator extends Component {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDateChange = this.handleDateChange.bind(this);
-    this.handleMachineChange = this.handleMachineChange.bind(this);
-    this.handleSecondChange = this.handleSecondChange.bind(this);
-    this.handleMinuteChange = this.handleMinuteChange.bind(this);
-    this.handleDistanceChange = this.handleDistanceChange.bind(this);
-    this.handleUserChange = this.handleUserChange.bind(this);
-    this.handleCommentChange = this.handleCommentChange.bind(this); 
     this.state = {
       machineType: '',
       userName: '',
@@ -29,43 +23,6 @@ class CardioMachineCreator extends Component {
       startDate: date
     });
   };
-
-  handleMachineChange(machine) {
-    this.setState({
-      machineType: machine
-    });
-  };
-
-  handleSecondChange(seconds) {
-    this.setState({
-      durationSeconds: seconds
-    });
-  };
-
-  handleMinuteChange(minutes) {
-    this.setState({
-      durationMinutes: minutes
-    });
-  };
-
-  handleDistanceChange(miles) {
-    this.setState({
-      distanceMiles: miles
-    });
-  };
-
-  handleUserChange(user) {
-    this.setState({
-      userName: user
-    });
-  };
-
-  handleCommentChange(comments) {
-    this.setState({
-      comment: comments
-    });
-  }; 
-  
 
   handleSubmit(event) {
     document.getElementById("submitCardioBtn").disabled = true;
@@ -111,20 +68,23 @@ class CardioMachineCreator extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="field is-horizontal">
             <label className="label" for="machineType">Machine Type &nbsp;</label>
-            <input className="input" name="machineType" defaultValue="" type="text" value={this.state.machineType} onChange={this.handleMachineChange} required />
+            <input className="input" 
+                   name="machineType" 
+                   type="text" 
+                   value={this.state.machineType} 
+                   onChange={ e => this.setState({ durationMinutes : e.target.value }) } 
+                   required />
           </div>
 
           <div className="field is-horizontal">
 	          <label className="label" for="startDate">Date &nbsp;</label>
-	          <DatePicker
-              selected={this.state.startDate}
-              onChange={this.handleDateChange}
-              showTimeSelect
-              timeFormat="HH:mm"
-              timeIntervals={5}
-              dateFormat="MMMM d, yyyy h:mm aa"
-              timeCaption="time"
-            />
+	          <DatePicker selected={this.state.startDate}
+                        onChange={this.handleDateChange}
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={5}
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                        timeCaption="time" />
           </div>
 
           <div className="field is-horizontal">
@@ -134,26 +94,51 @@ class CardioMachineCreator extends Component {
             <div className="field-body">
               <div className="field"> 
                 <label for="durationMinutes">Minutes</label>
-                <input className="input" name="durationMinutes" defaultValue="0" min="0" type="number" value={this.state.durationMinutes} onChange={this.handleMinuteChange} />
+                <input className="input" 
+                       name="durationMinutes" 
+                       min="0" 
+                       type="number" 
+                       value={this.state.durationMinutes} 
+                       onChange={ e => this.setState({ durationMinutes : e.target.value }) } />
               </div>
               <div className="field"> 
                 <label for="durationSeconds">Seconds</label>
-                <input className="input" name="durationSeconds" defaultValue="0" min="0" max="59" type="number" value={this.state.durationSeconds} onChange={this.handleSecondChange} />
+                <input className="input" 
+                       name="durationSeconds" 
+                       min="0" 
+                       max="59" 
+                       type="number"
+                       value={this.state.durationSeconds} 
+                       onChange={ e => this.setState({ durationSeconds : e.target.value }) } />
               </div>
             </div>
           </div>
 
           <div className="field is-horizontal">
             <label className="label" for="distanceMiles">Distance (miles) &nbsp;</label>
-            <input className="input" name="distanceMiles" defaultValue="0.0" type="number" step="0.01" value={this.state.distanceMiles} />
+            <input className="input" 
+                   name="distanceMiles" 
+                   type="number" 
+                   step="0.01" 
+                   value={this.state.distanceMiles}
+                   onChange={ e => this.setState({ distanceMiles : e.target.value }) } />
           </div>
           <div className="field is-horizontal">
             <label className="label" for="userName">User &nbsp;</label>
-            <input className="input" name="userName" defaultValue="" type="text" value={this.state.userName} onChange={this.handleUserChange} required />
+            <input className="input" 
+                   name="userName" 
+                   type="text" 
+                   value={this.state.userName} 
+                   onChange={ e => this.setState({ userName : e.target.value }) } 
+                   required />
           </div>
           <div className="field">
             <label className="label" for="comment">Comment &nbsp;</label>
-            <textarea className="textarea" name="comment"defaultValue="" type="textarea" value={this.state.comment} onChange={this.handleCommentChange} />
+            <textarea className="textarea" 
+                      name="comment" 
+                      type="textarea" 
+                      value={this.state.comment} 
+                      onChange={ e => this.setState({ userName : e.target.value }) } />
           </div>	    
           <div className="field">
             <div className="control">
