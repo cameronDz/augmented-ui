de47e387c3b8cd6a7a1fe636ecc91eb16e0a7603
Cameron Dziurgot <camerondziurgot@gmail.com>
2019-01-05 04:32:36 -0500

    Added dummy pagination buttons.

--- src/components/fitness/CardioMachineSessions.jsx
+++ src/components/fitness/CardioMachineSessions.jsx
@@ -83,7 +83,16 @@ class CardioMachineSessions extends Component {
             <tbody>
               {cardioMachineComponent}
             </tbody>
-          </table>
+	  </table>
+        </div>
+        <div className="table-pagination" aria-label="pagination">
+          <a className="pagination-link">1</a>
+          <span className="pagination-ellipsis">&hellip;</span>
+          <a className="pagination-link">2</a>
+          <a className="pagination-link is-current">3</a>
+          <a className="pagination-link">4</a>
+          <span className="pagination-ellipsis">&hellip;</span>
+          <a className="pagination-link">5</a>
         </div>
       </div>
     );
