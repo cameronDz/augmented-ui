a052bbeef73292933f3d36388e120980e82603f3
cameronDz <camerondziurgot@gmail.com>
2019-02-18 16:44:36 -0500

    Moving PageButton to own component file.

---
+++ src/components/fitness/CardioMachinePageButton.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchSessionsIfNeeded } from '../../actions/cardioMachineSessionAction';
+
+
+/**
+ * 
+ * @prop links Array of Strings
+ * @prop currentPage 
+ * @prop totalPages
+ */
+class CardioMachinePageButton extends Component {
+
+    processPayload(payLoad)  {
+      console.log("cardioState: " + this.props.cardioState);
+      var array = [];
+      for(var i = 0; i < payLoad.length; i++) {
+        var counter = payLoad[i];
+        var obj = {};
+        obj.machineType = counter.machineType;
+        obj.startTime = counter.startTime;
+        obj.duration = counter.durationSeconds;
+        obj.distance = counter.distanceMiles;
+        obj.userName = counter.userName;
+        obj.comment = counter.comment;
+        array.push(obj);
+      }
+      return array;
+    }
+  
+    onPaginationClick(link) {
+        console.log('link: ' + link);
+        console.log('this: ' + this);
+      this.props.dispatchFetchSessions(link);
+      fetch(link)
+        .then(response => response.json())
+        .then(data => this.setState({
+          cardioSessions: this.processPayload(data.data),
+          currentPage: data.meta._currentPage,
+          totalPages: data.meta._totalPages,
+          links: data.links
+        })
+      );
+    };
+  
+    render() {
+      let classValues = "pagination-link";
+      if(this.props.currentPage === this.props.pageNumber) {
+        classValues += " is-current";
+      }
+  
+      return (
+        <a className={classValues} onClick={() => this.onPaginationClick(this.props.pageLink)}>
+          {this.props.pageNumber}
+        </a>
+      );
+    }
+}
+  
+const mapStateToProps = state =>  ({
+    cardioState: state.cardioMachineSession
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatchFetchSessions: (link) => {
+      dispatch(fetchSessionsIfNeeded(link));
+    }
+  };
+}
+  
+export default connect(mapStateToProps, mapDispatchToProps)(CardioMachinePageButton);
--- src/components/fitness/CardioMachineTablePagination.jsx
+++ src/components/fitness/CardioMachineTablePagination.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchSessionsIfNeeded } from '../../actions/cardioMachineSessionAction';
 
+import CardioMachinePageButton from './CardioMachinePageButton';
+
 /**
  * Triple dot between buttons when multiple pages inbetween next button.
  */
@@ -19,65 +21,9 @@ class PageEllipsis extends Component {
  * @prop currentPage 
  * @prop totalPages
  */
-class PageButton extends Component {
-
-  processPayload(payLoad)  {
-    console.log("cardioState: " + this.props.cardioState);
-    var array = [];
-    for(var i = 0; i < payLoad.length; i++) {
-      var counter = payLoad[i];
-      var obj = {};
-      obj.machineType = counter.machineType;
-      obj.startTime = counter.startTime;
-      obj.duration = counter.durationSeconds;
-      obj.distance = counter.distanceMiles;
-      obj.userName = counter.userName;
-      obj.comment = counter.comment;
-      array.push(obj);
-    }
-    return array;
-  }
-
-  onPaginationClick(link) {
-	  console.log('link: ' + link);
-	  console.log('this: ' + this);
-    this.props.dispatchFetchSessions(link);
-    fetch(link)
-      .then(response => response.json())
-      .then(data => this.setState({
-        cardioSessions: this.processPayload(data.data),
-        currentPage: data.meta._currentPage,
-        totalPages: data.meta._totalPages,
-        links: data.links
-      })
-    );
-  };
-
-  render() {
-    let classValues = "pagination-link";
-    if(this.props.currentPage === this.props.pageNumber) {
-      classValues += " is-current";
-    }
-
-    return (
-      <a className={classValues} onClick={() => this.onPaginationClick(this.props.pageLink)}>
-        {this.props.pageNumber}
-      </a>
-    );
-  }
-}
-
-/**
- * 
- * @prop links Array of Strings
- * @prop currentPage 
- * @prop totalPages
- */
 class CardioMachineTablePagination extends Component {
     
   render() {
-	  console.log("render");
-	  console.log("this: " + this);
     let pager;	  
 
     if (this.props.totalPages === 0 ) {
@@ -88,14 +34,14 @@ class CardioMachineTablePagination extends Component {
     } else if(this.props.totalPages === 1) {
       pager = (
         <div className="table-pagination" aria-label="pagination">      
-          <PageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
+          <CardioMachinePageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
         </div>            
       );        
     } else if(this.props.totalPages === 2) {
       pager = (
         <div className="table-pagination" aria-label="pagination">      
-          <PageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
-          <PageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />  
+          <CardioMachinePageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
+          <CardioMachinePageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />  
         </div>     
       );               
     } else if(this.props.totalPages === 3) {
@@ -110,27 +56,27 @@ class CardioMachineTablePagination extends Component {
       
       pager = (
         <div className="table-pagination" aria-label="pagination">      
-          <PageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
-          <PageButton pageLink={linkTwo} currentPage={this.props.currentPage} pageNumber={2} /> 
-          <PageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />  
+          <CardioMachinePageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
+          <CardioMachinePageButton pageLink={linkTwo} currentPage={this.props.currentPage} pageNumber={2} /> 
+          <CardioMachinePageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />  
         </div>     
       );   
     } else if(this.props.totalPages >= 4 && this.props.currentPage === 1) {
       pager = (
         <div className="table-pagination" aria-label="pagination">	      
-          <PageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} />
-          <PageButton pageLink={this.props.links.next} currentPage={this.props.currentPage} pageNumber={2} />
+          <CardioMachinePageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} />
+          <CardioMachinePageButton pageLink={this.props.links.next} currentPage={this.props.currentPage} pageNumber={2} />
           <PageEllipsis />
-          <PageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />	      
+          <CardioMachinePageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />	      
         </div>	      
       );	      
     } else if(this.props.totalPages >= 4 && this.props.currentPage === this.props.totalPages) {
       pager = (
         <div className="table-pagination" aria-label="pagination">        
-          <PageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} />
+          <CardioMachinePageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} />
           <PageEllipsis />
-          <PageButton pageLink={this.props.links.prev} currentPage={this.props.currentPage} pageNumber={this.props.totalPages - 1} />
-          <PageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />       
+          <CardioMachinePageButton pageLink={this.props.links.prev} currentPage={this.props.currentPage} pageNumber={this.props.totalPages - 1} />
+          <CardioMachinePageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />       
         </div>        
       );        
     } else if (this.props.totalPages === 4) {
@@ -146,74 +92,74 @@ class CardioMachineTablePagination extends Component {
       
       pager = (
         <div className="table-pagination" aria-label="pagination">        
-          <PageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} />
-          <PageButton pageLink={linkTwo} currentPage={this.props.currentPage} pageNumber={2} />
-          <PageButton pageLink={linkThree} currentPage={this.props.currentPage} pageNumber={3} />
-          <PageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={4} />       
+          <CardioMachinePageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} />
+          <CardioMachinePageButton pageLink={linkTwo} currentPage={this.props.currentPage} pageNumber={2} />
+          <CardioMachinePageButton pageLink={linkThree} currentPage={this.props.currentPage} pageNumber={3} />
+          <CardioMachinePageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={4} />       
         </div>        
       );  
     } else if (this.props.totalPages >= 5 && this.props.currentPage === 2) {
       pager = (
         <div className="table-pagination" aria-label="pagination">        
-          <PageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
-          <PageButton pageLink={this.props.links.self} currentPage={this.props.currentPage} pageNumber={2} />
-          <PageButton pageLink={this.props.links.next} currentPage={this.props.currentPage} pageNumber={3} />  
+          <CardioMachinePageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
+          <CardioMachinePageButton pageLink={this.props.links.self} currentPage={this.props.currentPage} pageNumber={2} />
+          <CardioMachinePageButton pageLink={this.props.links.next} currentPage={this.props.currentPage} pageNumber={3} />  
           <PageEllipsis />   
-          <PageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />  
+          <CardioMachinePageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />  
         </div>    
       );      
     } else if (this.props.totalPages >= 5 && this.props.currentPage === this.props.totalPages - 1) {
       pager = (
         <div className="table-pagination" aria-label="pagination">        
-          <PageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
+          <CardioMachinePageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
           <PageEllipsis />   
-          <PageButton pageLink={this.props.links.prev} currentPage={this.props.currentPage} pageNumber={this.props.totalPages - 2} />
-          <PageButton pageLink={this.props.links.self} currentPage={this.props.currentPage} pageNumber={this.props.totalPages - 1} />  
-          <PageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />  
+          <CardioMachinePageButton pageLink={this.props.links.prev} currentPage={this.props.currentPage} pageNumber={this.props.totalPages - 2} />
+          <CardioMachinePageButton pageLink={this.props.links.self} currentPage={this.props.currentPage} pageNumber={this.props.totalPages - 1} />  
+          <CardioMachinePageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />  
         </div>    
       );      
     } else if (this.props.totalPages === 5) {
       pager = (
         <div className="table-pagination" aria-label="pagination">        
-          <PageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} />
-          <PageButton pageLink={this.props.links.prev} currentPage={this.props.currentPage} pageNumber={2} />
-          <PageButton pageLink={this.props.links.self} currentPage={this.props.currentPage} pageNumber={3} />
-          <PageButton pageLink={this.props.links.next} currentPage={this.props.currentPage} pageNumber={4} />     
-          <PageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={5} />  
+          <CardioMachinePageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} />
+          <CardioMachinePageButton pageLink={this.props.links.prev} currentPage={this.props.currentPage} pageNumber={2} />
+          <CardioMachinePageButton pageLink={this.props.links.self} currentPage={this.props.currentPage} pageNumber={3} />
+          <CardioMachinePageButton pageLink={this.props.links.next} currentPage={this.props.currentPage} pageNumber={4} />     
+          <CardioMachinePageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={5} />  
         </div>        
       );  
     } else if (this.props.currentPage === 3) {
       pager = (
         <div className="table-pagination" aria-label="pagination">        
-          <PageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
-          <PageButton pageLink={this.props.links.prev} currentPage={this.props.currentPage} pageNumber={2} />
-          <PageButton pageLink={this.props.links.self} currentPage={this.props.currentPage} pageNumber={3} />
-          <PageButton pageLink={this.props.links.next} currentPage={this.props.currentPage} pageNumber={4} />  
+          <CardioMachinePageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
+          <CardioMachinePageButton pageLink={this.props.links.prev} currentPage={this.props.currentPage} pageNumber={2} />
+          <CardioMachinePageButton pageLink={this.props.links.self} currentPage={this.props.currentPage} pageNumber={3} />
+          <CardioMachinePageButton pageLink={this.props.links.next} currentPage={this.props.currentPage} pageNumber={4} />  
           <PageEllipsis />   
-          <PageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />  
+          <CardioMachinePageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />  
         </div>    
       );     
     } else if (this.props.currentPage === this.props.totalPages - 2) {
       pager = (
         <div className="table-pagination" aria-label="pagination">        
-          <PageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
+          <CardioMachinePageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} /> 
           <PageEllipsis />   
-          <PageButton pageLink={this.props.links.prev} currentPage={this.props.currentPage} pageNumber={this.props.totalPages - 3} />
-          <PageButton pageLink={this.props.links.self} currentPage={this.props.currentPage} pageNumber={this.props.totalPages - 2} />
-          <PageButton pageLink={this.props.links.next} currentPage={this.props.currentPage} pageNumber={this.props.totalPages - 1} />  
-          <PageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />  
+          <CardioMachinePageButton pageLink={this.props.links.prev} currentPage={this.props.currentPage} pageNumber={this.props.totalPages - 3} />
+          <CardioMachinePageButton pageLink={this.props.links.self} currentPage={this.props.currentPage} pageNumber={this.props.totalPages - 2} />
+          <CardioMachinePageButton pageLink={this.props.links.next} currentPage={this.props.currentPage} pageNumber={this.props.totalPages - 1} />  
+          <CardioMachinePageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={this.props.totalPages} />  
         </div>    
       );     
     } else {
       pager = (
         <div className="table-pagination" aria-label="pagination">        
-          <PageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} />
+          <CardioMachinePageButton pageLink={this.props.links.first} currentPage={this.props.currentPage} pageNumber={1} />
           <PageEllipsis />
-          <PageButton pageLink={this.props.links.prev} currentPage={this.props.currentPage} pageNumber={this.props.currentPage - 1} />
-          <PageButton pageLink={this.props.links.self} currentPage={this.props.currentPage} pageNumber={this.props.currentPage} />
-          <PageButton pageLink={this.props.links.next} currentPage={this.props.currentPage} pageNumber={this.props.currentPage + 1} />  
+          <CardioMachinePageButton pageLink={this.props.links.prev} currentPage={this.props.currentPage} pageNumber={this.props.currentPage - 1} />
+          <CardioMachinePageButton pageLink={this.props.links.self} currentPage={this.props.currentPage} pageNumber={this.props.currentPage} />
+          <CardioMachinePageButton pageLink={this.props.links.next} currentPage={this.props.currentPage} pageNumber={this.props.currentPage + 1} />  
           <PageEllipsis />   
-          <PageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={5} />  
+          <CardioMachinePageButton pageLink={this.props.links.last} currentPage={this.props.currentPage} pageNumber={5} />  
         </div>    
       );  
     }
@@ -230,12 +176,4 @@ const mapStateToProps = state =>  ({
   cardioState: state.cardioMachineSession
 });
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    dispatchFetchSessions: (link) => {
-      dispatch(fetchSessionsIfNeeded(link));
-    }
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CardioMachineTablePagination);
+export default connect(mapStateToProps, null)(CardioMachineTablePagination);
