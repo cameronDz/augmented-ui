b3b356e2b7ec5a867e9e5b00e30f52001011ee5c
cameronDz <camerondziurgot@gmail.com>
2019-06-26 20:19:55 -0400

    turn exercise creator from class based to functional component

--- src/js/components/fitness/ExerciseCreator.jsx
+++ src/js/components/fitness/ExerciseCreator.jsx
@@ -1,55 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import * as _config from '../../../../assets/data/config.json';
 
-class ExerciseCreator extends Component {
+// class ExerciseCreator extends Component {
+const exerciseCreator = () => {
 
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.name = React.createRef();
-    this.description = React.createRef();
-    this.typeId = React.createRef();
-  }
+  const submitButtonId = "exerciseCreatorSubmitButton";
+  const [description, setDescription] = useState('');
+  const [name, setName] = useState('');
+  const [typeId, setTypeId] = useState('');
 
-  handleSubmit(event) {
+  const resetFormValues = () => {
+    setDescription('');
+    setName('');
+    setTypeId('');
+  };
+
+  const handleSubmit = event => {
     event.preventDefault();
+    document.getElementById(submitButtonId).disabled = true;
 
-    const payload = JSON.stringify({
-      name: this.name.current.value,
-      description: this.description.current.value,
-      typeId: this.typeId.current.value
-    });
-
+    const header = { header : { "Content-Type": "application/json" } };
+    const payload = JSON.stringify({ description, name,  typeId });
     const url = _config.apis.azure + 'exercises';
-    var http = new XMLHttpRequest();
-    http.open('POST', url, true);
-    http.setRequestHeader('Content-type', 'application/json');
-    http.send(payload);
+    axios.post(url, payload, header)
+      .then(() => {
+        resetFormValues();
+      })
+      .catch(error => {
+        // TODO inform user
+        console.error(error);
+      })
+      .finally(() => {
+        document.getElementById(submitButtonId).disabled = false;
+    });
   }
 
-  render() {
-    return (
-      <div>
-        <p><strong>Create a new Exercise</strong></p>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label forHtml="name">Name:</label><br/>
-            <input name="name"defaultValue="" type="text" ref={this.name} />
-          </div>
-          <div>
-            <label forHtml="description">Description:</label><br/>
-            <textarea name="description" defaultValue="" ref={this.description}></textarea>
-          </div>
-          <div>
-            <label forHtml="typeId">Type:</label><br/>
-            <input name="typeId" defaultValue="1" type="number" ref={this.typeId} />
-          </div>
-          <div>
-            <input type="submit" value="Submit" />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
-export default ExerciseCreator;
+  return (
+    <div>
+      <p><strong>Create a new Exercise</strong></p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label forHtml="name">Name:</label><br/>
+          <input defaultValue="" name="name" onChange={event => setName(event.target.value)} type="text" />
+        </div>
+        <div>
+          <label forHtml="description">Description:</label><br/>
+          <textarea defaultValue="" name="description" onChange={event => setDescription(event.target.value)} >
+          </textarea>
+        </div>
+        <div>
+          <label forHtml="typeId">Type:</label><br/>
+          <input defaultValue="1" name="typeId" onChange={event => setTypeId(event.target.value)} type="number" />
+        </div>
+        <div>
+          <input id={submitButtonId} type="submit" value="Submit" />
+        </div>
+      </form>
+    </div>);
+};
+
+export default exerciseCreator;
