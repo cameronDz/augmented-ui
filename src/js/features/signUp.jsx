8a88a7624f0fd897c4daf081b9cf86acf24b731a
Cam Dziurgot <camerondziurgot@gmail.com>
2019-10-27 00:30:56 -0400

    move sign in/up pages to feature dir

---
+++ src/js/features/signUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './layout';
+import Card from '../components/bulmaCard';
+
+const signUp = () => {
+  const cardContent = (<p>Use this page to sign up for application.</p>);
+  return (
+    <Layout title='Sign Up for Augmented'>
+      <Card child={cardContent} title='Sign Up' />
+    </Layout>);
+}
+
+export default signUp;
