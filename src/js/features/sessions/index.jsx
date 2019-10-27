b276b53fd293cbf6a8eac02e4f2384d8e18f09af
Cam Dziurgot <camerondziurgot@gmail.com>
2019-10-26 20:00:34 -0400

    add call to get sessions from session page index

--- src/js/features/sessions/index.jsx
+++ src/js/features/sessions/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import Page from '../../containers/page';
 import SessionCreator from '../../components/fitness/cardioMachine/sessionCreator';
 import SessionsTableDisplay from '../../components/fitness/cardioMachine/sessionsTableDisplay';
+import { fetchSessionsIfNeeded } from '../../state/cardioMachineSessions/actions';
+import * as _config from '../../../../assets/data/config.json';
 
-const sessions = () => {
+const sessions = props => {
   const tabNames = ['Information', 'Record', 'History', 'Data Visualization'];
   const tabPanels = [
     <p key={1}>Information on fitness sessions</p>,
@@ -11,7 +14,13 @@ const sessions = () => {
     <SessionsTableDisplay key={3}/>,
     <p key={4}>Under construction</p>
   ];
+
+  useEffect(() => {
+    const apiUrl = _config.apis.azure + 'CardioMachineExercises?pageNumber=1&pageSize=10';
+    props.fetchSessionsIfNeeded(apiUrl);
+  }, []);
+
   return <Page title="Sessions" tabNames={tabNames} tabPanels={tabPanels} />;
 };
 
-export default sessions;
+export default connect(null, { fetchSessionsIfNeeded})(sessions);
