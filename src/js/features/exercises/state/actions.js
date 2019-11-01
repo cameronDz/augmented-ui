1c0d95030b48b88f8148e2b869f63247f631f97b
Cam Dziurgot <camerondziurgot@gmail.com>
2019-11-01 19:20:45 -0400

    create dispatches for exercise actions

--- src/js/features/exercises/state/actions.js
+++ src/js/features/exercises/state/actions.js
@@ -1,31 +1,33 @@
+import axios from 'axios';
+import * as _config from '../../../../../assets/data/config.json';
 import * as _types from './types';
 
-// actions for getting exercise list
-const invalidateExerciseList = () => {
-  return { type: _types.INVALIDATED_EXERCISE_LIST };
-};
-
-const loadExerciseList = () => {
-  return { type: _types.LOADING_EXERCISE_LIST };
-};
-
-const recieveExerciseList = () => {
-  return { type: _types.RECIEVE_EXERCISE_LIST };
+const recieveExerciseList = data => {
+  return { data, type: _types.RECIEVE_EXERCISE_LIST };
 };
 
 const requestExerciseList = () => {
-  return { type: _types.REQUEST_EXERCISE_LIST };
+  return dispatch => {
+    dispatch({ type: _type.LOADING_EXERCISE_LIST });
+    const config = { header: { 'Content-Type': 'application/json' } };
+    return axios.get(url, config).then(payload => {
+      dispatch(recieveExerciseList(payload.data));
+    });
+  };
 };
 
 // actions for creating new exercise
-const createNewExercisePost = () => {
-  return { type: _types.CREATE_NEW_EXERCISE_POST };
+
+const recieveNewExerciseResponse = response => {
+  return { response, type: _types.RECIEVE_NEW_EXERCISE_RESPONSE };
 };
 
-const recieveNewExerciseResponse = () => {
-  return { type: _types.RECIEVE_NEW_EXERCISE_RESPONSE };
+const createNewExercisePost = payload => {
+  dispatch({ type: _types.CREATE_NEW_EXERCISE_POST });
+  const config = { header: { 'Content-Type': 'application/json' } };
+  return axios.post(url, config, payload).then(response => {
+    dispatch(recieveNewExerciseResponse(response));
+  });
 };
 
-const waitForNewExerciseResponse = () => {
-  return { type: _types.WAIT_NEW_EXERCISE_RESPONSE };
-};
+export { createNewExercisePost, requestExerciseList };
