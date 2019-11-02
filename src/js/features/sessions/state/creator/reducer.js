505f6e770b23d37d38430c52e76e86dfc4d2e550
Cam Dziurgot <camerondziurgot@gmail.com>
2019-11-02 11:11:05 -0400

    add new creator reducer for sending post requests

---
+++ src/js/features/sessions/state/creator/reducer.js
@@ -0,0 +1,25 @@
+import initialState from './initialState';
+import * as _types from '../types';
+
+const reducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case _types.UPDATE_CARDIO_MACHINE_SESSION_POST_RESPONSE:
+      newState = { ...state, posting: false, request: { ...state.request, ...update } };
+      break
+    case _types.RECIEVE_CARDIO_MACHINE_SESSION_POST_RESPONSE:
+      newState = { ...state, posting: false };
+      break
+    case _types.RECIEVE_SUCCESSFUL_CARDIO_MACHINE_SESSION_POST_RESPONSE:
+      newState = { ...initialState, posting: false, successfulPost: true };
+      break
+    case _types.SEND_CARDIO_MACHINE_SESSION_POST_REQUEST:
+      newState = { ...state, posting: true, successfulPost: false };
+      break;
+    default:
+      newState = { ...state };
+  }
+  return newState;
+};
+
+export default reducer;
--- src/js/state/rootReducer.js
+++ src/js/state/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import cardioMachineSessionsReducer from '../features/sessions/state/reducer';
+import cardioMachineSessionPostReducer from '../features/sessions/state/creator/reducer';
 import caffeineIntakeReducer from '../features/nutrients/caffeine/state/reducer';
 import exerciseListReducer from '../features/exercises/state/reducer';
 import rountineReducer from '../features/routines/state/reducer';
@@ -7,6 +8,7 @@ import rountineReducer from '../features/routines/state/reducer';
 export default combineReducers({
   caffeineIntakes: caffeineIntakeReducer,
   cardioMachineSessions: cardioMachineSessionsReducer,
+  cardioMachineSessionPost: cardioMachineSessionPostReducer,
   exercises: exerciseListReducer,
   routine: rountineReducer
 });
