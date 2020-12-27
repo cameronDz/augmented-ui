e6da6b80aecabc2b28b5d9135a11e0e313e346c5
cameronDz <camerondziurgot@gmail.com>
2020-12-27 03:42:34 -0500

    install webpack copy module

--- webpack/common.babel.js
+++ webpack/common.babel.js
@@ -1,3 +1,4 @@
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 import HtmlWebPackPlugin from 'html-webpack-plugin';
 import { resolve } from 'path';
 
@@ -29,6 +30,18 @@ module.exports = {
     }]
   },
   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: resolve(__dirname, '..', './src/assets/images'),
+          to: 'images'
+        },
+        {
+          from: resolve(__dirname, '..', './src/favicon.ico'),
+          to: 'favicon.ico'
+        }
+      ]
+    }),
     new HtmlWebPackPlugin({
       filename: resolve(__dirname, '..', './dist/index.html'),
       template: resolve(__dirname, '..', './src/index.html')
