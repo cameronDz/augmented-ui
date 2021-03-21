d2ddca84f9b4f632ed8f2c328dc74a4757053373
cameronDz <camerondziurgot@gmail.com>
2021-03-20 21:10:02 -0400

    set actions for s3 storage api

--- src/js/features/sessions/state/actions.js
+++ src/js/features/sessions/state/actions.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
-import * as types from './types';
+import * as _types from './types';
+import * as _config from '../../../../../assets/config.json';
 import { shouldFetchState } from '../../../state/global';
+
+const httpHeader = { header: { 'Content-Type': 'application/json' } };
+const cardioSessionGetPath = 'json/object/cardio';
+const cardioSessionPutPath = 'json/update/cardio';
+
+const emitDispatch = (type, actions = {}) => {
+  return { type, ...actions };
+};
 
 // get session actions
 const fetchSessions = url => {
@@ -13,7 +22,7 @@ const fetchSessions = url => {
 };
 
 const requestCardioMachineSessions = sessionApiUrl => {
-  return { sessionApiUrl, type: types.REQUEST_CARDIO_MACHINE_SESSIONS };
+  return { sessionApiUrl, type: _types.REQUEST_CARDIO_MACHINE_SESSIONS };
 };
 
 const recieveCardioMachineSessions = payload => {
@@ -22,7 +31,7 @@ const recieveCardioMachineSessions = payload => {
     linkPayload: payload.data.links,
     metaPayload: payload.data.meta,
     receivedAt: Date.now(),
-    type: types.RECIEVE_CARDIO_MACHINE_SESSIONS
+    type: _types.RECIEVE_CARDIO_MACHINE_SESSIONS
   };
 };
 
@@ -31,7 +40,7 @@ const shouldFetchSessions = (state = {}) => {
 };
 
 export const invalidateCardioMachineSession = () => {
-  return { type: types.INVALIDATED_CARDIO_MACHINE_SESSIONS };
+  return { type: _types.INVALIDATED_CARDIO_MACHINE_SESSIONS };
 };
 
 export const fetchSessionsIfNeeded = (url = '') => {
@@ -44,3 +53,45 @@ export const fetchSessionsIfNeeded = (url = '') => {
     }
   };
 };
+
+const getCardioSessionList = () => {
+  return (dispatch) => {
+    const url = _config.apis.heroku + cardioSessionGetPath;
+    dispatch(emitDispatch(_types.GET_REQUEST_CARDIO_SESSION_LIST_START));
+    return axios.get(url, httpHeader)
+      .then((response) => {
+        const cardio = !!response && !!response.data && response.data.payload && Array.isArray(response.data.payload.cardio) ? response.data.payload.cardio : [];
+        return dispatch(emitDispatch(_types.GET_REQUEST_CARDIO_SESSION_LIST_SUCCESS, { data: cardio }));
+      })
+      .catch((error) => {
+        return dispatch(emitDispatch(_types.GET_REQUEST_CARDIO_SESSION_LIST_ERROR, { error }));
+      })
+      .finally(() => {
+        return dispatch(emitDispatch(_types.GET_REQUEST_CARDIO_SESSION_LIST_COMPLETED));
+      });
+  };
+};
+
+const putCardioSession = payload => {
+  return (dispatch) => {
+    const url = _config.apis.heroku + cardioSessionPutPath;
+    dispatch(emitDispatch(_types.PUT_REQUEST_CARDIO_SESSION_ITEM_START));
+    return axios.put(url, payload, httpHeader)
+      .then((response) => {
+        const type = !!response && !!response.data ? _types.PUT_REQUEST_CARDIO_SESSION_ITEM_SUCCESS : _types.PUT_REQUEST_CARDIO_SESSION_ITEM_ERROR;
+        const payload = {
+          data: !!response && !!response.data ? response.data : null,
+          error: 'No data in response.'
+        };
+        return dispatch(emitDispatch(type, payload));
+      })
+      .catch((error) => {
+        return dispatch(emitDispatch(_types.PUT_REQUEST_CARDIO_SESSION_ITEM_ERROR, { error }));
+      })
+      .finally(() => {
+        return dispatch(emitDispatch(_types.PUT_REQUEST_CARDIO_SESSION_ITEM_COMPLETED));
+      });
+  };
+};
+
+export { getCardioSessionList, putCardioSession };
