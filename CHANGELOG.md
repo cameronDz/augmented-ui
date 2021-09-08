f5216a09c94ac955a4937b4db99d4e71268f71c3
cameronDz <camerondziurgot@gmail.com>
2021-09-08 03:18:19 -0400

    add routing hash

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.13.0] -2021-09-08
+
+### Added
+
+- Hash in routes
+
 ### Changed
 
 - Naming updated
--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch as RouterSwtich } from 'react-router-dom';
 
 import ExercisePage from './features/exercises';
 import Footer from './components/footer';
@@ -12,14 +12,16 @@ const app = () => {
   return (
     <Fragment>
       <div className="app-root app-switch-container">
-        <Switch>
-          <Route exact path='/' component={Session} />
-          <Route exact path='/exercise' component={ExercisePage} />
-          <Route exact path='/help' component={Help} />
-          <Route exact path='/nutrient' component={NutrientPage} />
-          <Route exact path='/routine' component={RoutinePage} />
-          <Route exact path='/session' component={Session} />
-        </Switch>
+        <HashRouter>
+          <RouterSwtich>
+            <Route exact path='/' component={Session} />
+            <Route exact path='/exercise' component={ExercisePage} />
+            <Route exact path='/help' component={Help} />
+            <Route exact path='/nutrient' component={NutrientPage} />
+            <Route exact path='/routine' component={RoutinePage} />
+            <Route exact path='/session' component={Session} />
+          </RouterSwtich>
+        </HashRouter>
       </div>
       <Footer className="app-root-footer" />
     </Fragment>);
