7f17fc710a3106f8218accdb4f775d22b6331896
cameronDz <camerondziurgot@gmail.com>
2021-10-06 00:07:37 -0400

    make date order sort generic

--- src/js/lib/sorts.js
+++ src/js/lib/sorts.js
@@ -1,17 +1,19 @@
-const orderIntakesByDate = (a, b) => {
+const orderByDateKey = (a, b, key) => {
   let orderValue = 0;
-  try {
-    const aTime = new Date(a.intakeTime);
-    const bTime = new Date(b.intakeTime);
-    if (aTime < bTime) {
-      orderValue = 1;
-    } else if (aTime > bTime) {
-      orderValue = -1;
+  if (key) {
+    try {
+      const aTime = new Date(a?.[key]).getTime();
+      const bTime = new Date(b?.[key]).getTime();
+      if (aTime < bTime) {
+        orderValue = 1;
+      } else if (aTime > bTime) {
+        orderValue = -1;
+      }
+    } catch (err) {
+      console.error('could not properly order intakes due to error:', err);
     }
-  } catch (err) {
-    console.error('could not properly order intakes due to error:', err);
   }
   return orderValue;
 };
 
-export { orderIntakesByDate };
+export { orderByDateKey };
