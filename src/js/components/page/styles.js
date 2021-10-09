21c1fafee49217d9d1723f36a5d77098cbfdb2e5
cameronDz <camerondziurgot@gmail.com>
2021-10-08 20:17:40 -0400

    set up tabbed page

---
+++ src/js/components/page/styles.js
@@ -0,0 +1,13 @@
+const tabbedPageStyles = {
+  pageRoot: {
+    margin: '0 auto',
+    maxWidth: '1280px'
+  },
+  pageHero: {
+    fontSize: '26px',
+    padding: '16px',
+    textAlign: 'center'
+  }
+};
+
+export { tabbedPageStyles };
---
+++ src/js/components/page/tabbedPage.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {
+  makeStyles,
+  AppBar,
+  Grid,
+  Tab,
+  Tabs
+} from '@material-ui/core';
+import SimpleNavbar from '../simpleNavbar';
+import TabPanel from './tabPanel';
+import { tabbedPageStyles as styles } from './styles';
+
+const propTypes = {
+  isSecuredUser: PropTypes.bool,
+  tabNames: PropTypes.array.isRequired,
+  tabPanels: PropTypes.array.isRequired,
+  title: PropTypes.string
+};
+
+const useStyles = makeStyles(() => styles);
+const TabbedPage = ({ isSecuredUser, tabNames, tabPanels, title }) => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_event, newValue) => {
+    setValue(newValue);
+  };
+
+  const renderTabLabels = () => {
+    return !!Array.isArray(tabNames) && tabNames.map((item, key) => {
+      return <Tab key={key} label={item} />;
+    });
+  };
+
+  const renderTabPanels = () => {
+    return !!Array.isArray(tabPanels) && tabPanels.map((item, key) => {
+      return (<TabPanel index={key} key={key} value={value}>{item}</TabPanel>);
+    });
+  };
+
+  const classes = useStyles();
+  return (
+    <Grid className={classes.pageRoot} container>
+      <Grid item xs={12} sm={12} md={1}></Grid>
+      <Grid item xs={12} sm={12} md={10}>
+        <SimpleNavbar isSecuredUser={isSecuredUser} />
+        <h2 className={classes.pageHero}>{title}</h2>
+        <AppBar position="static">
+          <Tabs centered={true} value={value} onChange={handleChange} >
+            {renderTabLabels()}
+          </Tabs>
+        </AppBar>
+        {renderTabPanels()}
+      </Grid>
+      <Grid item xs={12} sm={12} md={1}></Grid>
+    </Grid>);
+};
+
+TabbedPage.propTypes = propTypes;
+const mapStateToProps = (state) => ({ isSecuredUser: !!state.auth.token });
+export default connect(mapStateToProps)(TabbedPage);
