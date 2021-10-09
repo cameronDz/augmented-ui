680e06b13ef6da466266a71615f71ba92446dcfe
cameronDz <camerondziurgot@gmail.com>
2021-10-09 03:55:28 -0400

    rm form from redux

--- src/js/state/rootReducer.js
+++ src/js/state/rootReducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import authReducer from '../auth/state/reducer';
 import cardioMachineSessionsReducer from '../features/sessions/state/reducer';
-import cardioMachineSessionPostReducer from '../features/sessions/state/creator/reducer';
 import caffeineIntakeReducer from '../features/nutrients/caffeine/state/reducer';
 import exerciseListReducer from '../features/exercises/state/reducer';
 import rountineReducer from '../features/routines/state/reducer';
@@ -10,7 +9,6 @@ export default combineReducers({
   auth: authReducer,
   caffeineIntakes: caffeineIntakeReducer,
   cardioMachineSessions: cardioMachineSessionsReducer,
-  cardioMachineSessionPost: cardioMachineSessionPostReducer,
   exercises: exerciseListReducer,
   routine: rountineReducer
 });
