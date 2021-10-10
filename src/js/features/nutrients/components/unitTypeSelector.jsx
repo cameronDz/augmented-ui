108fe0afea31488fb8e8dfb5c2d9018981d48f1b
cameronDz <camerondziurgot@gmail.com>
2021-10-10 18:17:44 -0400

    mv scaffold outline for unit selector

--- src/js/features/nutrients/components/unitTypeSelector.jsx
+++ src/js/features/nutrients/components/unitTypeSelector.jsx
@@ -1,36 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormControl, MenuItem, Select } from '@material-ui/core';
-import { eventDefaultValue } from '../../../lib/defaultValue';
+import { OutlinedSelector } from '../../../components/inputs';
 import { handleFunction } from '../../../lib/eventHandler';
 
+const unitOptions = [
+  { id: 'mg', name: 'mg' },
+  { id: 'pill', name: 'pill' },
+  { id: 'ounce', name: 'oz' },
+  { id: 'pound', name: 'lb' },
+  { id: 'kg', name: 'kg' }
+];
 const propTypes = {
   isDisabled: PropTypes.bool,
   onChange: PropTypes.func,
   value: PropTypes.string
 };
 const UnitTypeSelector = ({ isDisabled = true, onChange = null, value = '' }) => {
-  const handleChange = (event) => {
-    const newValue = eventDefaultValue(event, '');
-    handleFunction(onChange, newValue);
-  };
-
   return (
-    <FormControl variant="outlined">
-      <Select
-        disabled={isDisabled}
-        onChange={handleChange}
-        title="units"
-        value={value}
-      >
-        <MenuItem value=""><em>- -</em></MenuItem>
-        <MenuItem value="mg">mg</MenuItem>
-        <MenuItem value="pill">pill</MenuItem>
-        <MenuItem value="ounce">oz</MenuItem>
-        <MenuItem value="pound">lb</MenuItem>
-        <MenuItem value="kg">kg</MenuItem>
-      </Select>
-    </FormControl>
+    <OutlinedSelector
+      isDisabled={isDisabled}
+      label="Units"
+      onChange={(selected) => handleFunction(onChange, selected)}
+      options={unitOptions}
+      value={value}
+    />
   );
 };
 
