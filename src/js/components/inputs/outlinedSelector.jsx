97034c3d3fc9280648bc16216fadc1e093333681
cameronDz <camerondziurgot@gmail.com>
2021-10-10 18:01:29 -0400

    handle race cond

--- src/js/components/inputs/outlinedSelector.jsx
+++ src/js/components/inputs/outlinedSelector.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { FormControl, MenuItem, Select } from '@material-ui/core';
-import { eventDefaultValue } from '../../../lib/defaultValue';
-import { handleFunction } from '../../../lib/eventHandler';
+import { eventDefaultValue } from '../../lib/defaultValue';
+import { handleFunction } from '../../lib/eventHandler';
 
 const propTypes = {
   isDisabled: PropTypes.bool,
@@ -19,6 +19,7 @@ const OutlinedSelector = ({
   value = ''
 }) => {
   const [items, setItems] = useState([]);
+  const [selected, setSelected] = useState('');
 
   useEffect(() => {
     const newItems = [];
@@ -35,17 +36,26 @@ const OutlinedSelector = ({
     setItems(newItems);
   }, [options]);
 
+  useEffect(() => {
+    let newValue = '';
+    if (items?.length > 0) {
+      newValue = value;
+    }
+    setSelected(newValue);
+  }, [items, value]);
+
   const handleChange = (event) => {
     const newValue = eventDefaultValue(event, '');
     handleFunction(onChange, newValue);
   };
+
   return (
     <FormControl variant="outlined">
       <Select
         disabled={isDisabled}
         onChange={handleChange}
         title="units"
-        value={value}
+        value={selected}
       >
         <MenuItem value=""><em>- -</em></MenuItem>
         {items}
